bb6afc1899c1ef75a30a34d17a119dd031fa45d8
Александр Кравченков <sprintend@gmail.com>
2017-08-27 12:30:41 +0300

    Added other parsers

---
+++ APIBlueprintParser/APIBlueprintParser/Parsers/MainParser.cs
@@ -0,0 +1,59 @@
+﻿//
+// MainParser.cs
+// 26.08.2017
+// Created by Kravchenkov Alexander
+// sprintend@gmail.com
+//
+//
+
+using System;
+using System.IO;
+using APIBlueprintParser.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace APIBlueprintParser.Parsers {
+    
+    public class MainParser: BaseParser {
+        
+        public MainParser(StreamReader reader): base(reader) { }
+
+        public ApiDescription Parse() {
+            
+            var metadata = new MetadataParser(base.streamReader).Parse();
+            var overview = new OverviewParser(base.streamReader).Parse();
+
+            var result = new ApiDescription();
+
+            result.Metadata = new MetadataNode(metadata);
+            result.Overview = overview;
+
+            var groups = new List<GroupNode>();
+
+            var line = base.streamReader.ReadLine();
+
+            do
+            {
+
+                if (line == null)
+                {
+                    result.Groups = groups;
+                    return result;
+                }
+
+                if (!line.Contains("#"))
+                {
+                    line = base.streamReader.ReadLine();
+                }
+
+                if (line.Contains("#"))
+                {
+                    var res = new GroupParser(base.streamReader, line).Parse();
+                    line = res.lastReaded;
+                    groups.Add(res.groupNode);
+                }
+
+            } while (true);
+        }
+    }
+}
